4f5c3da1f6aa9f87b37d408b4085c3f1d0d5d3af
Capa1221 <Johannycarr@hotmail.com>
2024-10-24 15:02:13 -0500

    Cambios 2.0

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,48 +1,35 @@
-import { useState } from 'react';
-import { Text, View, StyleSheet, Pressable } from 'react-native';
+import {useState} from 'react'
+import{Text, View, StyleSheet,Pressable} from 'react-native'
+
 
 const CounterScreen = () => {
-    const [count, setCount] = useState(10);
-    
-    const handleDecrement = () => {
-        if (count > 0) {
-            setCount(count - 1); 
-        }
-    };
+    const [count, setCount] =useState(10);
+  return (
+    <View style={style.container}>
+        <Text style={style.title}>{count}</Text>
+        <Pressable
+        onPress={()=>setCount(count+1)}
+        onLongPress={()=> setCount(0)}
+        >
+        <Text>+1</Text>
+        
+        </Pressable>
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{count}</Text>
-            <Pressable
-                onPress={() => setCount(count + 1)}
-            >
-          
-                <Text>+1</Text>
-            </Pressable>
-        </View>
-    );
-};
+    </View>
+  )
+}
+const style= StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    title:{
+      fontSize:80,
+      color:'black',
+      fontWeight:'300'
+    }
+  
+  })
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 80,
-        color: 'black',
-        fontWeight: '300',
-    },
-    button: {
-        padding: 20,
-        borderRadius: 5,
-        marginTop: 20,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 24,
-    },
-});
-
-export default CounterScreen;
+export default CounterScreen
